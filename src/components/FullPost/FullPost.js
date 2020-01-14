ac3766f9c12d96735ab909cb2b30a8b213cee853
vinay <ypvinayreddy@gmail.com>
2020-01-14 13:39:56 -0700

    add post delete functionality

---
+++ src/components/FullPost/FullPost.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+
+import './FullPost.css';
+import axios from'axios';
+
+class FullPost extends Component {
+    state = {
+        loadPost: null
+    }
+    componentDidUpdate() {
+        if(this.props.id) {
+            if(!this.state.loadPost || (this.state.loadPost && this.state.loadPost.id !== this.props.id)) {
+                debugger;
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id )
+                .then(response => {
+                    console.log("response", response);
+                  this.setState({loadPost : response.data });
+                })
+            }
+            
+        }
+    };
+
+    deletePostHandler = () => {
+        axios.delete('https://jsonplaceholder.typicode.com/posts/' + this.props.id )
+        .then(response => {
+            console.log(response)
+        })
+    }
+    render () {
+        let post = <p style={{ textAlign : 'center'}}>Please select a Post!</p>;
+        if(this.props.id) {
+            post = <p style={{ textAlign : 'center'}}>loading</p>;
+        }
+        if(this.state.loadPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadPost.title}</h1>
+                    <p>{this.state.loadPost.content}</p>
+                    <div className="Edit">
+                        <button onClick={this.deletePostHandler} className="Delete">Delete</button>
+                    </div>
+                </div>
+    
+            );
+        }
+        return post;
+    }
+}
+
+export default FullPost;
